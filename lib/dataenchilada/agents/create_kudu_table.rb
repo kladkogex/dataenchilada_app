3b045fa24f9b378c5f3d31dbe68a8ca12dcc87cc
elvis <alex.danko@galacticexchange.io>
2017-08-04 11:06:31 +0300

    fix for demo (kudu table from serioja)

--- lib/dataenchilada/agents/create_kudu_table.rb
+++ lib/dataenchilada/agents/create_kudu_table.rb
@@ -96,13 +96,13 @@ module Dataenchilada::Agents
           fingerprint BIGINT,
           time BIGINT,
           href STRING,
-          userAgent STRING,
+          user_agent STRING,
           browser STRING,
-          browserVersion STRING,
+          browser_version STRING,
           os STRING,
-          screenPrint STRING,
+          screen_print STRING,
           plugins STRING,
-          timeZone STRING,
+          time_zone STRING,
           language STRING,
           host STRING,
           port STRING,
@@ -110,35 +110,35 @@ module Dataenchilada::Agents
           width INT,
           referrer STRING,
           device STRING,
-          keyCode INT,
-          keyChar STRING,
-          timeDown BIGINT,
-          targetDownId STRING,
-          targetDownTagName STRING,
-          targetDownText STRING,
-          targetDownPath STRING,
-          timeUp BIGINT,
-          targetUpId STRING,
-          targetUpTagName STRING,
-          targetUpText STRING,
-          targetUpPath STRING,
+          key_code INT,
+          key_char STRING,
+          time_down BIGINT,
+          target_down_id STRING,
+          target_down_tag_name STRING,
+          target_down_text STRING,
+          target_down_path STRING,
+          time_up BIGINT,
+          target_up_id STRING,
+          target_up_tag_name STRING,
+          target_up_text STRING,
+          target_up_path STRING,
           x INT,
           y INT,
           moved BOOLEAN,
-          targetId STRING,
-          targetTagName STRING,
-          targetText STRING,
-          targetPath STRING,
-          doAlpha DOUBLE,
-          doBeta DOUBLE,
-          doGamma DOUBLE,
-          doAbsolute BOOLEAN,
-          dmX DOUBLE,
-          dmY DOUBLE,
-          dmZ DOUBLE,
-          dmGx DOUBLE,
-          dmGy DOUBLE,
-          dmGz DOUBLE,
+          target_d STRING,
+          target_tag_name STRING,
+          target_text STRING,
+          target_path STRING,
+          do_alpha DOUBLE,
+          do_beta DOUBLE,
+          do_gamma DOUBLE,
+          do_absolute BOOLEAN,
+          dm_x DOUBLE,
+          dm_y DOUBLE,
+          dm_z DOUBLE,
+          dm_gx DOUBLE,
+          dm_gy DOUBLE,
+          dm_gz DOUBLE,
           PRIMARY KEY(kudu_id, kudu_processed_at))
           PARTITION BY HASH PARTITIONS 16
           STORED AS KUDU
@@ -173,74 +173,3 @@ module Dataenchilada::Agents
 
   end
 end
-=begin
-    def self.get_tables(impala_host, impala_port)
-      connect = Impala.connect(impala_host, impala_port)
-      items = connect.execute("
-          show tables;
-      ")
-
-      connect.close
-      items
-    end
-    def self.get_tables(impala_host, impala_port)
-
-      %w(asdf asdf asdf)
-    end
-
-
-
-
-
-
-      # set client
-      client = Elasticsearch::Client.new trace: true, host: elastic_host, port: elastic_port
-
-      # check index exists
-      exists = client.indices.exists? index: index
-      # create new index
-      client.indices.create index: index, type: type unless exists
-
-      # mapping index
-      client.indices.put_mapping index: index, type: type, body: {
-          "#{type}" => {
-              dynamic: true,
-              properties: {
-                  :version => {:type => 'long', :index => :not_analyzed},
-                  :uptime => {:type => 'long', :index => :not_analyzed},
-                  :flow_records => {:type => 'long', :index => :not_analyzed},
-                  :flow_seq_num => {:type => 'long', :index => :not_analyzed},
-                  :engine_type => {:type => 'long', :index => :not_analyzed},
-                  :engine_id => {:type => 'long', :index => :not_analyzed},
-                  :sampling_algorithm => {:type => 'long', :index => :not_analyzed},
-                  :sampling_interval => {:type => 'long', :index => :not_analyzed},
-                  :ipv4_src_addr => {:type => 'ip', :index => :not_analyzed},
-                  :ipv4_dst_addr => {:type => 'ip', :index => :not_analyzed},
-                  :ipv4_next_hop => {:type => 'ip', :index => :not_analyzed},
-                  :input_snmp => {:type => 'long', :index => :not_analyzed},
-                  :output_snmp => {:type => 'long', :index => :not_analyzed},
-                  :in_pkts => {:type => 'long', :index => :not_analyzed},
-                  :in_bytes => {:type => 'long', :index => :not_analyzed},
-                  :first_switched => {:type => 'long', :index => :not_analyzed},
-                  :last_switched => {:type => 'long', :index => :not_analyzed},
-                  :l4_src_port => {:type => 'long', :index => :not_analyzed},
-                  :l4_dst_port => {:type => 'long', :index => :not_analyzed},
-                  :tcp_flags => {:type => 'long', :index => :not_analyzed},
-                  :protocol => {:type => 'long', :index => :not_analyzed},
-                  :src_tos => {:type => 'long', :index => :not_analyzed},
-                  :src_as => {:type => 'long', :index => :not_analyzed},
-                  :dst_as => {:type => 'long', :index => :not_analyzed},
-                  :src_mask => {:type => 'long', :index => :not_analyzed},
-                  :dst_mask => {:type => 'long', :index => :not_analyzed},
-                  :host => {:type => 'string', :index => :not_analyzed},
-                  :process_time => {:type => 'date', :index => :not_analyzed},
-                  :processed_at => {:type => 'date', :index => :not_analyzed},
-              }
-          }
-      }
-
-    end
-  end
-end
-
-=end
